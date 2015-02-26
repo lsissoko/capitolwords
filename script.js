8f69f2f71a336141fddde548245ac3bab2e78097
Lamine Sissoko <lsissoko91@gmail.com>
2015-02-25 22:08:44 -0500

    null data handling + JS indent size (now 4 spaces)

--- script.js
+++ script.js
@@ -1,91 +1,118 @@
 "use strict";
 
 $(document).ready(function() {
-  // adding trim() to String objects
-  if(typeof(String.prototype.trim) === "undefined") {
-    String.prototype.trim = function() {
-      return String(this).replace(/^\s+|\s+$/g, "");
-    };
-  }
-
-  $(":input[name=phrase]").keyup(function(e) {
-    if(e.keyCode === 13) {
-      $("#enter").click();
-    }
-  });
-
-  $("#enter").click(function(e) {
-    e.preventDefault();
-
-    var _phrase = $(":input[name=phrase]").val();
-    if (_phrase.trim().length === 0) {
-      return;
+    // add trim() to String objects
+    if (typeof(String.prototype.trim) === "undefined") {
+        String.prototype.trim = function() {
+            return String(this).replace(/^\s+|\s+$/g, "");
+        };
     }
 
-    $.ajax(
-      "http://capitolwords.org/api/dates.json", {
-        type: "GET",
-        dataType: "jsonp",
-        data: {
-          apikey: "f6ab5f2e4f69444b9f2c0a44d9a5223d",
-          phrase: _phrase,
-          percentages: true,
-          granularity: "year"
+    $(":input[name=phrase]").keyup(function(e) {
+        if (e.keyCode === 13) {
+            $("#enter").click();
         }
-      }
-    ).done(function(data) {
-      chart(data, _phrase);
-    });
-  });
-
-  function chart(data, phrase) {
-    var lineData = [];
-    var xData = [];
-    var yData = [];
-
-    $.each(data.results, function(i, item) {
-      lineData.push({
-        x: item.year,
-        y: item.count
-      });
     });
 
-    $.each(lineData, function(i, item) {
-      xData.push(item.x);
-      yData.push(item.y);
-    });
+    $("#enter").click(function(e) {
+        e.preventDefault();
 
-    $("#chart-container").highcharts({
-      chart: {
-        type: "line"
-      },
-      title: {
-        text: phrase
-      },
-      xAxis: {
-        categories: xData
-      },
-      yAxis: {
-        title: {
-          text: "Number of Mentions"
-        },
-        floor: 0
-      },
-      plotOptions: {
-        line: {
-          enableMouseTracking: true
+        var _phrase = $(":input[name=phrase]").val();
+        if (_phrase.trim().length === 0) {
+            return;
         }
-      },
-      series: [{
-        name: phrase,
-        data: yData
-      }],
-      credits: {
-        enabled: false
-      }
-    });
-  }
 
-  // show results for "congress"
-  $("#enter").click();
+        $.ajax(
+            "http://capitolwords.org/api/dates.json", {
+                type: "GET",
+                dataType: "jsonp",
+                data: {
+                    apikey: "f6ab5f2e4f69444b9f2c0a44d9a5223d",
+                    phrase: _phrase,
+                    percentages: true,
+                    granularity: "year"
+                }
+            }
+        ).done(function(data) {
+            chart(data.results, _phrase);
+        });
+    });
+
+    function chart(data, phrase) {
+        var lineData = [];
+        var xData = [];
+        var yData = [];
+
+        // get years and counts from AJAX response data
+        $.each(data, function(i, item) {
+            lineData.push({
+                x: item.year,
+                y: item.count
+            });
+        });
+
+        // deal with missing data points
+        var MIN_YEAR = 1996;
+        var MAX_YEAR = new Date().getFullYear();
+        for (var k = MIN_YEAR; k <= MAX_YEAR; k++) {
+            var found = false;
+            for (var j = 0; j < lineData.length; j++) {
+                if (lineData[j].x === ("" + k)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                lineData.push({
+                    x: k,
+                    y: 0
+                });
+            }
+        }
+
+        // sort data by year
+        lineData.sort(function(a, b) {
+            return a.x - b.x;
+        })
+
+        // put year and count data into separate arrays
+        $.each(lineData, function(i, item) {
+            xData.push(item.x);
+            yData.push(item.y);
+        });
+
+        // draw the chart
+        $("#chart-container").highcharts({
+            chart: {
+                type: "line"
+            },
+            title: {
+                text: phrase
+            },
+            xAxis: {
+                categories: xData
+            },
+            yAxis: {
+                title: {
+                    text: "Number of Mentions"
+                },
+                floor: 0
+            },
+            plotOptions: {
+                line: {
+                    enableMouseTracking: true
+                }
+            },
+            series: [{
+                name: phrase,
+                data: yData
+            }],
+            credits: {
+                enabled: false
+            }
+        });
+    }
+
+    // show default results ("congress") on page load
+    $("#enter").click();
 });
