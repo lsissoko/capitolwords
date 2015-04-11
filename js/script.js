86a9aa7e76c2138e0a8505b20b8489804d8fab6a
Lamine Sissoko <lsissoko91@gmail.com>
2015-04-11 00:45:22 -0400

    Move drawChart() to its own file

--- js/script.js
+++ js/script.js
@@ -29,92 +29,6 @@ $(document).ready(function() {
     return terms;
   }
 
-  function drawChart(data, term) {
-    var years = [];
-    var seriesData = [];
-    var MIN_YEAR = 1996;
-    var MAX_YEAR = new Date().getFullYear();
-
-    var y = MIN_YEAR;
-    while (y <= MAX_YEAR) {
-      years.push(y++);
-    }
-
-    $.each(data, function(i, termData) {
-      var curData = [];
-      var counts = [];
-
-      // get the current term's year and count data
-      $.each(termData.results, function(j, item) {
-        curData.push({
-          x: item.year,
-          y: item.count
-        });
-      });
-
-      // fill missing data (count = 0 for that year)
-      for (var year = MIN_YEAR; year <= MAX_YEAR; year++) {
-        var found = false;
-        for (var j = 0, l = curData.length; j < l; j++) {
-          if (curData[j].x === ("" + year)) {
-            found = true;
-            break;
-          }
-        }
-        if (!found) {
-          curData.push({
-            x: year,
-            y: 0
-          });
-        }
-      }
-
-      // sort the data by year
-      curData.sort(function(a, b) {
-        return a.x - b.x;
-      });
-
-      // put the count data in its own array
-      $.each(curData, function(j, item) {
-        counts.push(item.y);
-      });
-
-      // add new series object
-      seriesData.push({
-        name: termData.term,
-        data: counts
-      });
-    });
-
-    // draw the chart
-    $("#chart-container").highcharts({
-      chart: {
-        type: "line"
-      },
-      title: {
-        text: term
-      },
-      xAxis: {
-        categories: years
-      },
-      yAxis: {
-        title: {
-          text: "Number of Mentions"
-        },
-        floor: 0
-      },
-      plotOptions: {
-        line: {
-          enableMouseTracking: true
-        }
-      },
-      series: seriesData,
-      credits: {
-        enabled: false
-      }
-    });
-  }
-
   function search(terms) {
     var data = [];
     var requests = [];
@@ -143,7 +57,7 @@ $(document).ready(function() {
     });
 
     $.when.apply(undefined, requests).then(function(r) {
-      drawChart(data);
+      drawChart(data, "#chart-container");
     });
   }
 
